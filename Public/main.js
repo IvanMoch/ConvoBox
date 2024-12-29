21a5b84f4b94e2f2954fa83e6943120297ce3a53
IvanMoch <mochniyivan@gmail.com>
2024-12-29 17:26:58 -0400

    suggested rooms for user added to the view

--- Public/main.js
+++ Public/main.js
@@ -1,5 +1,4 @@
 
-console.log(userID)
 
 //This function logs out the user and erase the session cookie
 function logout() {
@@ -36,4 +35,31 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
 })
-        
+
+
+// this function show all the suggested rooms for the user
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(`/api/room/suggestedRooms`)
+        .then((res) => {
+            if (res.ok) {
+                return res.json()
+            }
+            console.log('error')
+        })
+        .then((rooms) => {
+            if (rooms.length === 0) {
+                document.getElementById('suggested-groups').innerHTML = '<h1>No suggested rooms</h1>'
+            } else {
+                rooms.forEach((room) => {
+                    document.getElementById('suggested-groups').innerHTML += `
+                    <div class="group-card
+                    " id="${userID}">
+                        <h3>${room.name}</h3>
+                        <p>${room.description}</p>
+                        <button>Enter Group</button>
+                    </div>`
+                })
+            }
+        })
+})
